2811f636e373856d0e2d107a17cf528f5efea8f2
Riya Deshpande <riyadeshpande10@gmail.com>
2025-05-12 22:48:43 -0400

    Updates

---
+++ precache-manifest.c072e40ce9a2fec9d08164d0d5b1b684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d44cba5cb29a7eef1ffa2c74a588a81",
+    "url": "/CS_571_beanRating/index.html"
+  },
+  {
+    "revision": "594001d3e884e95a0a33",
+    "url": "/CS_571_beanRating/static/css/main.9aa2f136.chunk.css"
+  },
+  {
+    "revision": "420a1f679a8a54a9fbee",
+    "url": "/CS_571_beanRating/static/js/2.1b3a424a.chunk.js"
+  },
+  {
+    "revision": "cc3e2937ba864a85aa22",
+    "url": "/CS_571_beanRating/static/js/3.9be12790.chunk.js"
+  },
+  {
+    "revision": "594001d3e884e95a0a33",
+    "url": "/CS_571_beanRating/static/js/main.56cd4e02.chunk.js"
+  },
+  {
+    "revision": "8aa574c991ccd26da006",
+    "url": "/CS_571_beanRating/static/js/runtime~main.b6a256ec.js"
+  }
+]);
